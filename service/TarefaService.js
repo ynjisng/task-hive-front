a87d3783aa9dc7ccaa85339368a3c012c2265659
ynjisng <abreusara012@gmail.com>
2023-04-09 23:24:04 -0300

    atualizando deps

---
+++ service/TarefaService.js
@@ -0,0 +1,22 @@
+import http from "@/common/http";
+
+class TarefaService {
+    create(data) {
+        console.log(data);
+        return http.post("/tarefa", data);
+    }
+    edit(data) {
+        console.log(data);
+        return http.post("/tarefa" + data.id, data);
+    }
+    list() {
+        return http.get("/tarefa");
+    }
+    delete(id) {
+        return http.delete("/tarefa/" + id);
+    }
+    load(id) {
+        return http.get("/tarefa/" + id);
+    }
+}
+export default new TarefaService();
