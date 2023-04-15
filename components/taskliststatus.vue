831aed22541830ebd79bac972a479b5c3839dc00
ynjisng <abreusara012@gmail.com>
2023-04-15 11:56:52 -0300

    salvando inicio da criação do quadro de tarefas

---
+++ components/taskliststatus.vue
@@ -0,0 +1,35 @@
+<template>
+    <v-container class="bg-surface-variant">
+        <v-row no-gutters class="mb-6">
+            <v-col class="pa-2 ma-2" v-for="(status, index) in statuses" :key="index" cols="3">
+                <h2>{{ status }}</h2>
+                <v-card v-for="(task, i) in tasks" :key="i" class="mb-4" v-if="task.statuses === status">
+                    <v-card-title>{{ task.titulo }}</v-card-title>
+                    <v-card-text>{{ task.descricao }}</v-card-text>
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+  
+<script>
+export default {
+    props: {
+        tasks: {
+            default: () => []
+        },
+        statuses: {
+            default: () => []
+        }
+    }
+}
+</script>
+
+<style>
+.col-3 {
+    background-color: lightgray;
+    border-radius: 5px;
+}
+
+</style>
+  
--- pages/tarefas/_tarefaprojeto.vue
+++ pages/tarefas/_tarefaprojeto.vue
@@ -2,43 +2,16 @@
     <div>
         <h1>Tarefa Projeto {{ tarefas.projeto }}</h1>
         <p>{{ this.$route.params.tarefaprojeto }}</p>
+        <taskliststatus :tasks="tarefas" :statuses="statuses" />
         <v-container class="bg-surface-variant">
-            <v-row class="mb-6" no-gutters>
-                <v-col>
-                    <v-sheet class="pa-2 ma-2">
-                        STATUS
-                    </v-sheet>
-                </v-col>
-
-                <v-col>
-                    <v-sheet class="pa-2 ma-2">
-                        STATUS
-                    </v-sheet>
-                </v-col>
-
-                <v-col>
-                    <v-sheet class="pa-2 ma-2">
-                        STATUS
-                    </v-sheet>
-                </v-col>
-
-                <v-col>
-                    <v-sheet class="pa-2 ma-2">
-                        STATUS
-                    </v-sheet>
-                </v-col>
-            </v-row>
         </v-container> <!-- VERFICAR SOBRE OS IDENTIFICADORES DOS STATUS -->
 
-        <div>
-            <v-expansion-panels variant="inset" class="my-4">
-                <v-expansion-panel v-for="tarefa in tarefas" 
-                :key="tarefa.id"
-                title="tarefa.titulo"
+
+        <v-expansion-panels variant="inset" class="my-4">
+            <v-expansion-panel v-for="tarefa in tarefas" :key="tarefa.id" title="tarefa.titulo"
                 text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.">
-                </v-expansion-panel>
-            </v-expansion-panels>
-        </div>
+            </v-expansion-panel>
+        </v-expansion-panels>
     </div>
 </template>
 
@@ -53,11 +26,15 @@ const route = useRoute()
 console.log(route.params.tarefaprojeto)
 const id_projeto = route.params.tarefaprojeto;
 const tarefas = ref({});
+const statuses = ['pendente', 'top', 'topado']
 
 function load() {
     TarefaService.list(null).then(
         response => {
             tarefas.value = response.data.filter(tarefa => tarefa.projeto.id == id_projeto);
+            tarefas.value.forEach((tarefa, index) => {
+                tarefa.statuses = statuses[index]
+            }); 
             console.log(tarefas.value)
         }
     )
